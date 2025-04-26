bb93ecae7e2d76584a7db8ff0d64373e081b1887
Justin Grote <JustinGrote@users.noreply.github.com>
2025-04-26 01:34:32 +0000

    👷 Consolidate Docker files and enable multi arch builds

--- build.ps1
+++ build.ps1
@@ -3,26 +3,18 @@ using namespace System.Management.Automation
 [CmdletBinding()]
 param(
 	#The minimum version to build for. This should be increased when EOL dates are reached.
-	[SemanticVersion]$minimumPSVersion = '7.4',
-	$PSVersionToNetFrameworkMap = @{
-		'7.4' = '8.0.14'
-		'7.5' = '9.0.3'
-		'7.6' = '9.0.3'
-		#TODO: Pre-fetch the zip to determine .NET framework version maybe?
-		'7.5.0-preview.1' = '8.0.14'
-	},
+	[SemanticVersion]$minimumPSVersion = '7.5',
 	#For now, our build process is the same for both. This may change in the future
 	$Distributions = @(
-		'azurelinux3.0-distroless'
 		'noble-chiseled'
+		'azurelinux3.0-distroless'
 	),
 	#Map the architectures to the release nomenclature we use.
-	$Architectures = [ordered]@{
-		'amd64' = $null #Use Default
-		'arm64' = 'powershell-{0}-linux-arm64.tar.gz'
-	},
-	$LocalRepo = 'powershell',
-	$RemoteRepo = 'ghcr.io/justingrote/powershell'
+	[string[]]$Architectures = @('amd64', 'arm64'),
+	$LocalImageName = 'powershell',
+	$remoteImageName = 'ghcr.io/justingrote/powershell',
+	#Push to remote repo
+	[switch]$Push
 )
 
 $irmParams = @{
@@ -38,6 +30,7 @@ $releases = Invoke-RestMethod @irmParams -Verbose:$false
 # Gather releases newer than specified minimumPSVersion
 $pwshReleases = $releases
 | Select-Object id,
+tag_name,
 @{
 	N = 'Version';
 	E = { [SemanticVersion]($_.tag_name -replace '^v') }
@@ -51,122 +44,143 @@ assets
 [System.Collections.Generic.HashSet[string]]$latestTag = @()
 
 #Generate the Azure Linux Distroless Images
-foreach ($arch in $Architectures.Keys) {
+foreach ($release in $pwshReleases) {
 	foreach ($distribution in $Distributions) {
-		foreach ($release in $pwshReleases) {
-			[SemanticVersion]$version = $release.Version
-			[string]$releaseMajorMinorVersion = $version.Major, $version.Minor -join '.'
+		[SemanticVersion]$version = $release.Version
+		[string]$releaseMajorMinorVersion = $version.Major, $version.Minor -join '.'
 
-			Write-Verbose "🟢: Building PowerShell $version - $distribution - $arch"
+		$powershellTag = "$version-$distribution"
+		$powershellImageTag = "$LocalImageName`:$powershellTag"
+		$powershellRemoteTag = "$remoteImageName`:$powershellTag"
 
-			# Check for a specific version (usually for preview overrides), then check for the general feature release map
-			$dotnetVersion = $PSVersionToNetFrameworkMap[$version.ToString()] ??
-			$PSVersionToNetFrameworkMap[$releaseMajorMinorVersion]
+		Write-Verbose "🟢: Building PowerShell $powershellTag"
 
-			if (-not $dotnetVersion) {
-				Write-Error "No matching distro Image for PowerShell version $releaseMajorMinorVersion"
+		# Fetch the global.json from the commit to determine the .NET version
+		$dotnetVersion = (Invoke-RestMethod "https://raw.githubusercontent.com/PowerShell/PowerShell/refs/tags/$($release.tag_name)/global.json" -Verbose:$false).sdk.version
+
+		#Do some massaging to get the correct tag. For example convert 10.0.100-preview.2.25164.34 to 10.0-preview
+		$currentDotnetVersion = $dotnetVersion
+		$dotnetVersion = $dotnetVersion -replace '^(\d+\.\d+)(\.\d+)-(\w+)\.(\d+)?.+$', '$1.0-$3.$4'
+		if ($dotnetVersion -ne $currentDotnetVersion) {
+			Write-Debug "Dotnet Preview Version Detected. New Tag: $dotnetVersion"
+		} else {
+			[Version]$releaseVer = $dotnetVersion
+			$dotnetVersion = $releaseVer.Major, $releaseVer.Minor -join '.'
+			Write-Debug "Dotnet Version Detected. New Tag: $dotnetVersion"
+		}
+
+		if (-not $dotnetVersion) {
+			Write-Error "Unable to fetch .NET version for PowerShell version $releaseMajorMinorVersion"
+			continue
+		}
+
+		if (-not $dotnetVersion) {
+			Write-Error "No matching distro Image for PowerShell version $releaseMajorMinorVersion"
+			continue
+		}
+
+
+		Push-Location $PSScriptRoot/Containers/Distroless
+		try {
+			$platforms = $Architectures | ForEach-Object {
+				"linux/$_"
+			}
+
+			$podmanBuildArgs = @(
+				# Pass the version and distribution to the podmanfile
+				'--build-arg', "DIST=$distribution",
+				'--build-arg', "PS_VERSION=$version",
+				'--build-arg', "DOTNET_VERSION=$dotnetVersion"
+				'--platform', ($platforms -join ',')
+				'--manifest', $powershellImageTag
+			)
+
+
+			Write-Debug "podman Build Args: $($podmanBuildArgs -join ' ')"
+
+			& podman build @podmanBuildArgs . *>&1
+			| ForEach-Object {
+				$podmanLogs += $_
+				Write-Debug "${powershellTag}: $_"
+			}
+
+			if ($LASTEXITCODE -ne 0) {
+				Write-Error "podman Build Failed: `n $($podmanLogs -join '`n')"
 				continue
 			}
 
-			$imageArch = $arch
-			#HACK: dotnet doesnt provide a amd64 summary tag for azurelinux, only amd64v8. It does for chiseled
-
-			if ($arch -eq 'arm64') {
-				$imageArch = 'arm64v8'
-			}
-
-			$dotnetImageTag = "$dotnetVersion-$distribution-$imageArch"
-			$dotnetImageUri = "mcr.microsoft.com/dotnet/runtime:$dotnetImageTag"
-			$powershellImageTag = "$localRepo`:$version-dotnet-$dotnetImageTag"
-			$powershellRemoteTag = "$remoteRepo`:$version-dotnet-$dotnetImageTag"
-
-			# Quick sanity check the dotnet image exists
-			$manifest = & docker manifest inspect $dotnetImageUri
-			if ($LASTEXITCODE -eq 1) {
-				Write-Error "Unable to find dotnet image $manifest"
+			#Basic Sanity Check
+			# TODO: Test in amd64 devcontainer
+			$testValue = 'ValidContainer'
+			$testResult = & podman run --rm $powershellImageTag "'$testValue'"
+			if ($testResult -ne $testValue) {
+				Write-Error "PowerShell Image $powershellImageTag failed basic PowerShell script test"
 				continue
 			}
 
-			Push-Location $PSScriptRoot/AzureLinux
-			try {
-				$dockerBuildArgs = @(
-					# Pass the version and distribution to the Dockerfile
-					'--build-arg', "IMAGE=$dotnetImageUri",
-					'--build-arg', "PS_VERSION=$version",
-					'-t', $powershellImageTag
-				)
-
-				# Use an alternate powershell zip if needed
-				if ($architectures[$arch]) {
-					$dockerBuildArgs += '--build-arg'
-					$dockerBuildArgs += $architectures[$arch] -f $version
+			#Check for rollup tag candidates. Since we start with Azure Linux, it will always default to those distro images first.
+			[string[]]$additionalTags = @()
+			if (-not $version.PreReleaseLabel -and -not $version.BuildLabel) {
+				if ($latestTag.Add('latest')) {
+					Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as latest"
+					$additionalTags += 'latest'
+				}
+				if ($latestTag.Add($Version.Major)) {
+					Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $($Version.Major)"
+					$additionalTags += $Version.Major
+				}
+				if ($latestTag.Add($releaseMajorMinorVersion)) {
+					Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $releaseMajorMinorVersion"
+					$additionalTags += $releaseMajorMinorVersion
+				}
+				if ($latestTag.Add($version)) {
+					Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $version"
+					$additionalTags += $version
 				}
 
-				$dockerLogs = & docker build @dockerBuildArgs . *>&1
-				docker tag $powershellImageTag $powershellRemoteTag
-
-				#Basic Sanity Check
-				# TODO: Test in amd64 devcontainer
-				if ($arch -eq 'amd64') {
-					$testValue = 'ValidContainer'
-					$testResult = & docker run --rm $powershellImageTag "'$testValue'"
-					if ($testResult -ne $testValue) {
-						Write-Error "PowerShell Image $powershellImageTag failed basic PowerShell script test"
-						continue
-					}
-				}
-
-				#Check for rollup tag candidates. Since we start with Azure Linux, it will always default to those distro images first. As we have also already
-				[string[]]$additionalTags = @()
-				if (-not $version.PreReleaseLabel -and -not $version.BuildLabel) {
-					if ($latestTag.Add('latest')) {
-						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as latest"
-						$additionalTags += 'latest'
-					}
-					if ($latestTag.Add($Version.Major)) {
-						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $($Version.Major)"
-						$additionalTags += $Version.Major
-					}
-					if ($latestTag.Add($releaseMajorMinorVersion)) {
-						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $releaseMajorMinorVersion"
-						$additionalTags += $releaseMajorMinorVersion
-					}
-					if ($latestTag.Add($version)) {
-						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $version"
-						$additionalTags += $version
-					}
-
-					#LTS tag processing
-					if ($Version.Minor % 2 -eq 0) {
-						$majorLtsTag = "$($Version.Major)-lts"
-						if ($latestTag.Add($majorLtsTag)) {
-							Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $majorLtsTag"
-							$additionalTags += $majorLtsTag
-							if ($latestTag.Add('lts')) {
-								Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as lts"
-								$additionalTags += 'lts'
-							}
+				#LTS tag processing
+				if ($Version.Minor % 2 -eq 0) {
+					$majorLtsTag = "$($Version.Major)-lts"
+					if ($latestTag.Add($majorLtsTag)) {
+						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $majorLtsTag"
+						$additionalTags += $majorLtsTag
+						if ($latestTag.Add('lts')) {
+							Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as lts"
+							$additionalTags += 'lts'
 						}
 					}
+				}
 
-					#Distro tag processing
-					if ($latestTag.Add($distribution)) {
-						Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $distribution"
-						$additionalTags += $distribution
+				#Distro tag processing
+				if ($latestTag.Add($distribution)) {
+					Write-Verbose "🎯 $powerShellImageTag will be additionally tagged as $distribution"
+					$additionalTags += $distribution
+				}
+			}
+
+			$psExtraTags = foreach ($tag in $additionalTags) {
+				"powershell:$tag"
+			}
+			podman tag $powershellImageTag @additionalTags
+
+			if ($Push) {
+				Write-Debug "Pushing $powershellImageTag to $powershellRemoteTag"
+				podman push $powershellImageTag $powershellRemoteTag
+
+				if ($psExtraTags) {
+					foreach ($tag in $additionalTags) {
+						Write-Debug "Pushing additional tag $tag to $psExtraTags"
+						podman push $powershellImageTag "$remoteImageName`:$tag"
 					}
 				}
-
-				foreach ($tag in $additionalTags) {
-				 docker tag $powershellImageTag "powershell:$tag"
-				 docker tag $powerShellImageTag "$remoteRepo`:$tag"
-				}
-			} catch {
-				Write-Host -Fore Magenta $dockerLogs
-				throw
-			} finally {
-				Write-Verbose "🤚: Building PowerShell $version - $Distribution - $arch"
-				Pop-Location
 			}
+
+		} catch {
+			Write-Host -Fore Magenta $podmanLogs
+			throw
+		} finally {
+			Write-Verbose "🤚: Building PowerShell $powershellTag"
+			Pop-Location
 		}
 	}
 }
